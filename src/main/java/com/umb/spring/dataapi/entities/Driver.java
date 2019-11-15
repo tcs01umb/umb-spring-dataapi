a3717f70d0161690112c24c0dcc27222a15c1b7c
dharma <tcs01_umb@aol.com>
2019-11-14 23:22:16 -0600

    added more fields

--- src/main/java/com/umb/spring/dataapi/entities/Driver.java
+++ src/main/java/com/umb/spring/dataapi/entities/Driver.java
@@ -5,7 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+import javax.persistence.TemporalType;
+import javax.persistence.Temporal;
 import com.umb.spring.dataapi.models.DriverRO;
 
 import java.util.Date;
@@ -18,11 +19,12 @@ public class Driver {
 	private String id;
 	private String firstName;
 	private String lastName;
-//	private Date dateOfBirth;
-//	private String gender;
-//	private String ssn;
-//	private String licenseNumber;
-//	private String licenseState;
+	@Temporal(TemporalType.DATE)
+	private Date dateOfBirth;
+	private String gender;
+	private String ssn;
+	private String licenseNumber;
+	private String licenseState;
 	public Driver() {
 		
 	}
@@ -30,11 +32,11 @@ public class Driver {
 		this.id = dr.getId();
 		this.firstName=dr.getFirstName();
 		this.lastName=dr.getLastName();
-//		this.dateOfBirth = dr.getDateOfBirth();
-//		this.gender=dr.getGender();
-//		this.ssn=dr.getSsn();
-//		this.licenseNumber=dr.getLicenseNumber();
-//		this.licenseState=dr.getLicenseState();
+		this.dateOfBirth = dr.getDateOfBirth();
+		this.gender=dr.getGender();
+		this.ssn=dr.getSsn();
+		this.licenseNumber=dr.getLicenseNumber();
+		this.licenseState=dr.getLicenseState();
 	}
 	public String getId() {
 		return id;
@@ -54,35 +56,35 @@ public class Driver {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-//	public Date getDateOfBirth() {
-//		return dateOfBirth;
-//	}
-//	public void setDateOfBirth(Date dateOfBirth) {
-//		this.dateOfBirth = dateOfBirth;
-//	}
-//	public String getGender() {
-//		return gender;
-//	}
-//	public void setGender(String gender) {
-//		this.gender = gender;
-//	}
-//	public String getSsn() {
-//		return ssn;
-//	}
-//	public void setSsn(String ssn) {
-//		this.ssn = ssn;
-//	}
-//	public String getLicenseNumber() {
-//		return licenseNumber;
-//	}
-//	public void setLicenseNumber(String licenseNumber) {
-//		this.licenseNumber = licenseNumber;
-//	}
-//	public String getLicenseState() {
-//		return licenseState;
-//	}
-//	public void setLicenseState(String licenseState) {
-//		this.licenseState = licenseState;
-//	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	public String getLicenseNumber() {
+		return licenseNumber;
+	}
+	public void setLicenseNumber(String licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+	public String getLicenseState() {
+		return licenseState;
+	}
+	public void setLicenseState(String licenseState) {
+		this.licenseState = licenseState;
+	}
 	
 }
--- src/main/java/com/umb/spring/dataapi/models/DriverRO.java
+++ src/main/java/com/umb/spring/dataapi/models/DriverRO.java
@@ -7,12 +7,12 @@ import com.umb.spring.dataapi.entities.Driver;
 public class DriverRO {
 	private String id;
 	private String firstName;
-	private String lastName;
-//	private Date dateOfBirth;
-//	private String gender;
-//	private String ssn;
-//	private String licenseNumber;
-//	private String licenseState;
+	private String lastName;	
+	private Date dateOfBirth;
+	private String gender;
+	private String ssn;
+	private String licenseNumber;
+	private String licenseState;
 	
 	public DriverRO() {
 		
@@ -26,11 +26,11 @@ public class DriverRO {
 		this.id = dr.getId();
 		this.firstName=dr.getFirstName();
 		this.lastName=dr.getLastName();
-//		this.dateOfBirth = dr.getDateOfBirth();
-//		this.gender=dr.getGender();
-//		this.ssn=dr.getSsn();
-//		this.licenseNumber=dr.getLicenseNumber();
-//		this.licenseState=dr.getLicenseState();
+		this.dateOfBirth = dr.getDateOfBirth();
+		this.gender=dr.getGender();
+		this.ssn=dr.getSsn();
+		this.licenseNumber=dr.getLicenseNumber();
+		this.licenseState=dr.getLicenseState();
 	}
 	public String getId() {
 		return id;
@@ -50,35 +50,35 @@ public class DriverRO {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-//	public Date getDateOfBirth() {
-//		return dateOfBirth;
-//	}
-//	public void setDateOfBirth(Date dateOfBirth) {
-//		this.dateOfBirth = dateOfBirth;
-//	}
-//	public String getGender() {
-//		return gender;
-//	}
-//	public void setGender(String gender) {
-//		this.gender = gender;
-//	}
-//	public String getSsn() {
-//		return ssn;
-//	}
-//	public void setSsn(String ssn) {
-//		this.ssn = ssn;
-//	}
-//	public String getLicenseNumber() {
-//		return licenseNumber;
-//	}
-//	public void setLicenseNumber(String licenseNumber) {
-//		this.licenseNumber = licenseNumber;
-//	}
-//	public String getLicenseState() {
-//		return licenseState;
-//	}
-//	public void setLicenseState(String licenseState) {
-//		this.licenseState = licenseState;
-//	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+	public String getLicenseNumber() {
+		return licenseNumber;
+	}
+	public void setLicenseNumber(String licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+	public String getLicenseState() {
+		return licenseState;
+	}
+	public void setLicenseState(String licenseState) {
+		this.licenseState = licenseState;
+	}
 	
 }
